fcf6f5dd3982eb6e6d692528856ebd08a07e056d
Richard Chukwuchekwa <richardchekwas@gmail.com>
2024-06-26 22:29:58 +0100

    update

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -9,7 +9,7 @@ const bodyParser = require('body-parser');
 
 const mapRoute = (app) => {
   app.use(cors({
-    origin: 'http://localhost:5173'
+    origin: 'http://167.99.194.130'
   }))
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());

ab24d256b547aae4f08e147ef7f23c5409cc7b60
Richard Chekwas <richardchekwas@gmail.com>
2025-06-21 16:57:57 +0100

    update

--- backend/t.js
+++ backend/t.js
@@ -1,32 +1,32 @@
 const multiply = (fst, snd) => {
         const str1 = (parseFloat(fst) * 100).toString();
-        const str2 = (parseFloat(snd) * 100).toString();
-        const ln1 = str1.length;
-        const ln2 = str2.length;
-        let mul = '';
-        let rem = '';
-        if (ln1 > ln2) {
-                for (let i = ln2 - 1; i >= 0; i--) {
-                        for (let j = (ln1 - 1); j >= 0; j--) {
-                                let m = parseInt(str2[i]) * parseInt(str1[j]);
-                                if (rem !== '') {
-                                        m = m + parseInt(rem);
-                                        rem = '';
-                                }
-                                if (m.toString().length === 1) {
-                                        mul = m.toString() + mul;
-                                } else if (m.toString().length > 1) {
-                                        mul = m.toString()[1] + mul;
-                                        rem = m.toString()[0];
-                                }
-                                if (j === 0 && rem !== '') {
-                                        mul = rem + mul;
-					rem = '';
-                                }
-                        }
-                        mul = '_' + mul;
-                }
-        }
+    const str2 = (parseFloat(snd) * 100).toString();
+    const ln1 = str1.length;
+    const ln2 = str2.length;
+    let mul = '';
+    let rem = '';
+    if (ln1 >= ln2) {
+            for (let i = ln2 - 1; i >= 0; i--) {
+                    for (let j = (ln1 - 1); j >= 0; j--) {
+                            let m = parseInt(str2[i]) * parseInt(str1[j]);
+                            if (rem !== '') {
+                                    m = m + parseInt(rem);
+                                    rem = '';
+                            }
+                            if (m.toString().length === 1) {
+                                    mul = m.toString() + mul;
+                            } else if (m.toString().length > 1) {
+                                    mul = m.toString()[1] + mul;
+                                    rem = m.toString()[0];
+                            }
+                            if (j === 0 && rem !== '') {
+                                    mul = rem + mul;
+                rem = '';
+                            }
+                    }
+                    mul = '_' + mul;
+            }
+    }
 	const addlst = mul.slice(1).split('_');
 	const addln = addlst.length;
 	let zeros = '';
@@ -40,24 +40,30 @@ const multiply = (fst, snd) => {
                         nwadd.push(addlst[i] + zeros);
 			zeros = zeros.slice(1);
                 }
-	console.log(nwadd);
-	nwadd.forEach((itm) => {
-		if (ans.length >= itm.length) {
-			ans = adding(ans, itm);
-		}
-		if (ans.length < itm.length) {
-			ans = adding(itm, ans);
-		}
-	});
-	let half = (parseFloat('.' + ans.slice(-4)).toFixed(2)).slice(-3);
+	    console.log(nwadd);
+	    nwadd.forEach((itm) => {
+		    if (ans.length >= itm.length) {
+			    ans = adding(ans, itm);
+		    }
+		    if (ans.length < itm.length) {
+			    ans = adding(itm, ans);
+	    	}
+	    });
+	    let half = (parseFloat('.' + ans.slice(-4)).toFixed(2)).slice(-3);
+	    if (half[2] === '0' && half[1] === '0') {
+		    half = '';
+	    } else if (half[2] === '0') {
+		    half = half.slice(0, -1);
+	    }
+	    return ans.slice(0, -4) + half;
+	}
+	let half = (parseFloat('.' + addlst[0].slice(-4)).toFixed(2)).slice(-3);
 	if (half[2] === '0' && half[1] === '0') {
 		half = '';
 	} else if (half[2] === '0') {
 		half = half.slice(0, -1);
 	}
-	return ans.slice(0, -4) + half;
-	}
-	return addlst[0];
+	return addlst[0].slice(0, -4) + half;
 };
 const adding = (fst, snd) => {
 	const ln1 = fst.length;
@@ -97,4 +103,4 @@ const adding = (fst, snd) => {
 	}
 	return addval
 };
-console.log(multiply('356', '78'), 'final');
+console.log(multiply('1.7', '1'), 'final');

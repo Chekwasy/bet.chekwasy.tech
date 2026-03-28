9c72b5f952136b0e5fc51eccd355281f64ad9fff
Richard Chekwas <richardchekwas@gmail.com>
2026-03-28 12:14:07 +0100

    update

--- frontend/web_vite_react/src/App.jsx
+++ frontend/web_vite_react/src/App.jsx
@@ -1,14 +1,29 @@
-import { useEffect, useState } from 'react';
-import { NavLink, Outlet, useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Outlet, useNavigate, useLocation } from 'react-router-dom';
 import Nav from './Nav.jsx';
 import Side_bar from './Side_bar.jsx';
 
-
 function App() {
   const navigate = useNavigate();
+  const location = useLocation();
+
   useEffect(() => {
-    navigate('/home', {replace: true});
+    // Detect page reload
+    const isReload =
+      performance.getEntriesByType("navigation")[0]?.type === "reload";
+
+    if (isReload && location.pathname !== "/") {
+      navigate("/", { replace: true });
+    }
   }, []);
+
+  useEffect(() => {
+    // Redirect root to /home
+    if (location.pathname === "/") {
+      navigate("/home", { replace: true });
+    }
+  }, [location.pathname]);
+
   return (
     <div className='all'>
       <Nav />
@@ -17,7 +32,7 @@ function App() {
         <Side_bar />
       </div>
     </div>
-  )
+  );
 }
 
 export default App;
